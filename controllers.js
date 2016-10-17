d9371c2abebe924572905468823704a972b6e80b
Adam Franzen <adamfranzen@gmail.com>
2016-10-17 12:02:24 -0600

    httpie commands working, scope issues with HTML

--- controllers.js
+++ controllers.js
@@ -1,28 +1,74 @@
 (function() {
   'use strict';
 
-  const app = angular.module('todoApp');
+  const server = 'http://localhost:3000';
 
-  app.controller('TodoListCtrl', function() {
-    this.todoToAdd = '';
-    this.todos = [];
+  angular
+  .module('todoApp')
+  .controller('PeopleCtrl', PeopleCtrl)
+  .controller('TodoListCtrl', TodoListCtrl)
 
-    this.addTodo = function(todoText) {
-      this.todos.push({
-        completed: false,
-        text: todoText
-      });
-      this.todoToAdd = '';
-    };
-  });
+  PeopleCtrl.$inject = ['$http', '$q'];
+  TodoListCtrl.$inject = ['$http', '$q'];
 
-  app.controller('PeopleCtrl', function() {
-    this.nameToAdd = '';
-    this.people = [];
 
-    this.addPerson = function(personName) {
-      this.people.push({ name: personName });
+  function PeopleCtrl($http, $q) {
       this.nameToAdd = '';
-    };
-  });
+      this.people = [];
+
+      const activate = () => {
+        console.log('activate');
+        return $http.get(`${server}/persons`)
+          .then((people) => {
+            this.people = people.data;
+
+            // Make requests for individual todos
+          })
+          .catch((err) => {
+            throw err;
+          });
+
+          let promises = this.people.map((person) => {
+            $http.get(`${server}/persons/${person.id}/todos`)
+              .then((todos) => {
+                console.log(this.people);
+                return person.todos = todos.data
+
+          })});
+
+          return $q.all(promises)
+      };
+
+      activate();
+
+      this.addPerson = () => {
+        return $http.post(`${server}/persons`, { name: this.nameToAdd })
+          .then((res) => {
+            res.data.todos = [];
+            this.people.push(res.data);
+            this.nameToAdd = '';
+          })
+          .catch((err) => {
+            throw err;
+          });
+      };
+    }
+
+    function TodoListCtrl($http) {
+      this.todoToAdd = '';
+
+      this.addTodo = (person) => {
+        return $http.post(`${server}/persons/${person.id}/todos`, {
+          completed: false,
+          text: this.todoToAdd
+        })
+        .then((res) => {
+          person.todos.push(res.data);
+          this.todoToAdd = '';
+        })
+        .catch((err) => {
+          throw err;
+        });
+      };
+    }
 }());
